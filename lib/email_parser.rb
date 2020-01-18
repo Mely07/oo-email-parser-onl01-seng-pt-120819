c5a56f41175d232c9b96cae17d9a35e0244d7404
Damely Tineo <damely.tineo07@gmail.com>
2020-01-18 02:13:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ class EmailAddressParser
   
   attr_accessor :email_addresses
   
-  @@useful_array = [ ]
+  @@useful_array 
   
   def initialize(email_addresses)
     @email_addresses = email_addresses
